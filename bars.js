993e4d3243451e7337a233309563074aa9debaa8
sudo-dave <david.macias.0101@gmail.com>
2021-10-01 23:04:03 -0700

    added style for swap aniamtion

--- bars.js
+++ bars.js
@@ -94,6 +94,58 @@ export class Algo {
 
     this.bars.swapBarsAnimaton(tempID, barObjects[i]["Id"]);
   }
+
+  async merge(arr, start, mid, end) {
+    let start2 = mid + 1;
+
+    // If the direct merge is already sorted
+    if (arr[mid] <= arr[start2]) {
+      return;
+    }
+
+    // Two pointers to maintain start
+    // of both arrays to merge
+    while (start <= mid && start2 <= end) {
+      // If element 1 is in right place
+      if (arr[start] <= arr[start2]) {
+        start++;
+      } else {
+        let value = arr[start2];
+        let index = start2;
+
+        // Shift all the elements between element 1
+        // element 2, right by 1.
+        while (index != start) {
+          arr[index] = arr[index - 1];
+          index--;
+        }
+        arr[start] = value;
+
+        // Update all the pointers
+        start++;
+        mid++;
+        start2++;
+      }
+    }
+  }
+  async mergeSort() {
+    let arr = this.bars.barObjects;
+    let l = arr[0]["Id"];
+    let r = arr.length - 1;
+
+    if (l < r) {
+      // Same as (l + r) / 2, but avoids overflow
+      // for large l and r
+      let m = l + Math.floor((r - l) / 2);
+
+      // Sort first and second halves
+      mergeSort(arr, l, m);
+      mergeSort(arr, m + 1, r);
+
+      merge(arr, l, m, r);
+    }
+  }
+
   async selectionSort() {
     let bars = this.bars;
     let barObjects = bars.barbarObjectsaules;
